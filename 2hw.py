3b901e311e326cfa9fc62df8c72b7c2d6bdf9bff
Maxon911 <55867945+Maxon911@users.noreply.github.com>
2019-11-07 00:01:27 +0500

    Create 2hw.py

---
+++ 2hw.py
@@ -0,0 +1,8 @@
+number1 = input("Введите действительное число 1 ")
+number1 = int(number1)
+number2 = input("Введите действительное число 2 ")
+number2 = int(number2)
+number3 = input("Введите действительное число 3 ")
+number3 = int(number3)
+print(max(number1, number2, number3))
+print(min(number1, number2, number3))
